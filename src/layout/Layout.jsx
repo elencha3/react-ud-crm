c668b10390c3a80830076483920affe7821489e8
Esther Leñador <e.lenadorc@hotmail.com>
2021-12-23 18:30:42 +0100

    sidebar & useLocation

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,44 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
-    return (
-        <div>
-            <h1>Desde Layout</h1>
-            <Outlet/>
-        </div>
-    )
-}
+    const location = useLocation();
 
-export default Layout
+    const currentUrl = location.pathname;
+
+    return (
+        <div className="md:flex md:min-h-screen">
+            <div className="md:w-1/5 bg-orange-400 px-5 py-10">
+                <h2 className="text-4xl font-black text-center text-gray-800 ">
+                    CRM-Clients
+                </h2>
+                <nav className="mt-10">
+                    <Link
+                        to="/clients"
+                        className={`${
+                            currentUrl === "/clients"
+                                ? "text-gray-100"
+                                : "text-gray-800"
+                        } text-2xl block mt-2 hover:text-gray-100 `}
+                    >
+                        Clients
+                    </Link>
+                    <Link
+                        to="/clients/new"
+                        className={`${
+                            currentUrl === "/clients/new"
+                                ? "text-gray-100"
+                                : "text-gray-800"
+                        } text-2xl block mt-2 hover:text-gray-100 `}
+                    >
+                        New Clients
+                    </Link>
+                </nav>
+            </div>
+            <div className="md:w-4/5">
+                <Outlet />
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
